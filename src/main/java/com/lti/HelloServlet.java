5e26d3664ca07450ff50940d2eeca1480704db49
ram <rams.11mca@gmail.com>
2020-11-19 00:30:25 +0530

    first commit

---
+++ src/main/java/com/lti/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.lti;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter pw = response.getWriter();
+		pw.println("welcome to servlets using maven");
+	}
+
+}
